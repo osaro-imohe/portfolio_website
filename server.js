4b0c37543e9d75fbcb9c4db4a31021025c343689
Osaro <osaroimohe99@yahoo.com>
2020-07-21 04:37:27 -0700

    installed node-fetch a dependency for the wakeUpDyno module

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 var path = require('path');
-const wakeUpDyno = require('wakeUpDyno.js');
+const wakeUpDyno = require('./wakeUpDyno');
 
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
