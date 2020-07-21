b46ae8aa34077a32b78658d542b2e9e88cf26db4
Osaro <osaroimohe99@yahoo.com>
2020-07-21 04:24:31 -0700

    made updates to the procfile settings

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'))
 app.get('*', (req, res) => res.status(404).sendFile(__dirname + '/html/404.html'));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen((process.env.PORT || 3000), () => {
     wakeUpDyno('https://osarospersonalwebsite.herokuapp.com/');
     console.log(`Example app listening at http://localhost:${port}`)
 })
